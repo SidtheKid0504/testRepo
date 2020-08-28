006d946c06cf89c8141c7ea2a459ac7f963d6e04
SidtheKid0504 <63013935+SidtheKid0504@users.noreply.github.com>
2020-08-28 10:34:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,38 +1,45 @@
-var map;
-var mapNumber;
-var mapSelect;
+var mapNumber, gameMap;
+var playerCount, player, allPlayers;
+var menu;
+var game;
+var gameState = 0;
+var player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18, player19, player20;
+var players;
+var randPlayer;
 var database;
+
 
 function preload(){
 
 }
 
 function setup() {
-    var canvas = createCanvas(displayWidth, displayHeight);
+    var canvas = createCanvas(displayWidth - 10, displayHeight - 10);
     database = firebase.database();
-    
-    
-    mapSelect = new Map();
-    mapNumber = Math.random(0, 4);
+    // mapNumber = Math.round(Math.random() * Math.floor(4));
+    game = new Game();
+    game.getGameState();
+    game.start();
 }
 
 function draw() {
-   mapSelect.selectMap(mapNumber);
-   mapSelect.getMap();
-
-    if (map === 0) {
-        background(0);
-    } else if (map === 1) {
-        background(255);
-    } else if (map === 2){
-        background(255, 0, 0);
-    } else if (map === 3) {
-        background(0, 255, 0);
-    } else if (map === 4) {
-        background(0, 0, 255);
+    
+    if (playerCount === 2 && gameState != 2) {
+        game.updateGameState(1);
     }
 
-    console.log(database);
+    if (gameState === 1) {
+        clear();
+        game.play();
+    }
 
-    drawSprites()
+    if (gameState === 2) {
+        game.end();
+    }
+
+    function touchStarted() {
+        console.log("Hi");
+    }
+
 }
+
